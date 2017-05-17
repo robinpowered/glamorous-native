2eb9d096ca0837cd357e1d695015a96be16b0d4c
Atticus White <contact@atticuswhite.com>
2017-05-16 21:54:48 -0400

    chore(build): commitizen and semantic-release
    Adds support for commitizen and semantic-release

--- package-scripts.js
+++ package-scripts.js
@@ -1,16 +1,21 @@
 const npsUtils = require('nps-utils')
 
 const concurrent = npsUtils.concurrent
+const series = npsUtils.series
 
 module.exports = {
   scripts: {
+    commit: {
+      description: 'This uses commitizen to help us generate well formatted commit messages',
+      script: 'git-cz',
+    },
     test: {
       default: 'jest --coverage',
       watch: 'jest --watch',
     },
     lint: {
       description: 'lint the entire project',
-      script: 'eslint ./src',
+      script: 'eslint .',
     },
     reportCoverage: {
       description: 'Report coverage stats to codecov. This should be run after the `test` script',
@@ -20,7 +25,23 @@ module.exports = {
       description: 'This runs several scripts to make sure thigns look good before committing or on clean install',
       default: concurrent.nps('lint', 'test'),
     },
+    release: {
+      description: 'We automate releases with semantic-release. This should only be run on travis',
+      script: series(
+        'semantic-release pre'
+
+        // 'npm publish',
+        // 'semantic-release post',
+      ),
+    },
     examples: {
+      reactNative: {
+        description: 'A simple react-native project using glamorous-native',
+        script: series(
+          'cd examples/react-native-glamorous',
+          'npm install'
+        ),
+      },
       // add yours here!
     },
   },
