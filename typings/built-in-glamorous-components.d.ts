d66562f70e3dfa7203b76b6cfb4618537ca848c1
Kalle Ott <kalle.ott@cap3.de>
2017-09-03 18:35:40 +0200

    added typescript declarations and package.json link

---
+++ typings/built-in-glamorous-components.d.ts
@@ -0,0 +1,59 @@
+import { ExtraGlamorousProps } from './glamorous-component'
+
+import {
+  ViewProperties,
+  ScrollViewStyle,
+  TextStyle,
+  ViewStyle,
+  ImageStyle,
+  TextInputProperties,
+  ImageProperties,
+  ListViewProperties,
+  ScrollViewProps,
+  TextProperties,
+  TouchableHighlightProperties,
+  TouchableNativeFeedbackProperties,
+  TouchableOpacityProperties,
+  TouchableWithoutFeedbackProps,
+  FlatListProperties,
+  SectionListProperties
+} from 'react-native'
+
+export interface NativeComponent {
+  Image: React.StatelessComponent<
+    ImageProperties & ExtraGlamorousProps & ImageStyle
+  >
+  ListView: React.StatelessComponent<
+    ListViewProperties & ExtraGlamorousProps & ViewStyle
+  >
+  ScrollView: React.StatelessComponent<
+    ScrollViewProps & ExtraGlamorousProps & ScrollViewStyle
+  >
+  Text: React.StatelessComponent<
+    TextProperties & ExtraGlamorousProps & TextStyle
+  >
+  TextInput: React.StatelessComponent<
+    TextInputProperties & ExtraGlamorousProps & TextStyle
+  >
+  TouchableHighlight: React.StatelessComponent<
+    TouchableHighlightProperties & ExtraGlamorousProps & ViewStyle
+  >
+  TouchableNativeFeedback: React.StatelessComponent<
+    TouchableNativeFeedbackProperties & ExtraGlamorousProps & ViewStyle
+  >
+  TouchableOpacity: React.StatelessComponent<
+    TouchableOpacityProperties & ExtraGlamorousProps & ViewStyle
+  >
+  TouchableWithoutFeedback: React.StatelessComponent<
+    TouchableWithoutFeedbackProps & ExtraGlamorousProps & ViewStyle
+  >
+  View: React.StatelessComponent<
+    ViewProperties & ExtraGlamorousProps & ViewStyle
+  >
+  FlatList: React.StatelessComponent<
+    FlatListProperties<any> & ExtraGlamorousProps & ViewStyle
+  >
+  SectionList: React.StatelessComponent<
+    SectionListProperties<any> & ExtraGlamorousProps & ViewStyle
+  >
+}
---
+++ typings/style-arguments.d.ts
@@ -0,0 +1,23 @@
+export interface StyleFunction<Properties, Props> {
+  (props: Props):
+    | Partial<Properties>
+    | string
+    | Array<Partial<Properties> | string | StyleFunction<Properties, Props>>
+}
+
+export type StyleArray<Properties, Props> = Array<
+  Partial<Properties> | string | StyleFunction<Properties, Props>
+>
+
+export type StaticStyleArray<Properties> = Array<Partial<Properties> | string>
+
+export type StyleArgument<Properties, Props> =
+  | Partial<Properties>
+  | string
+  | StyleFunction<Properties, Props>
+  | StyleArray<Properties, Props>
+
+export type StaticStyleArgument<Properties> =
+  | Partial<Properties>
+  | string
+  | StaticStyleArray<Properties>
